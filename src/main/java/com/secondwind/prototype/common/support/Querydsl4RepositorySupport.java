33c41e3c7c55fede172b30b72f9ea08326ba1aa7
conggooksi <kimgeonkuk@gmail.com>
2024-06-20 09:15:00 +0900

    :necktie: QueryDsl4RepositorySupport에 워닝들 제거

--- src/main/java/com/secondwind/prototype/common/support/Querydsl4RepositorySupport.java
+++ src/main/java/com/secondwind/prototype/common/support/Querydsl4RepositorySupport.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.lang.Assert;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.LongSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,88 +26,95 @@ import org.springframework.stereotype.Repository;
 @Repository
 public abstract class Querydsl4RepositorySupport {
 
-    private final Class domainClass;
+  private final Class<?> domainClass;
 
-    private Querydsl querydsl;
+  private Querydsl querydsl;
 
-    private EntityManager entityManager;
+  private EntityManager entityManager;
 
-    private JPAQueryFactory queryFactory;
+  private JPAQueryFactory queryFactory;
 
-    private boolean hasNext;
+  private boolean hasNext;
 
-    public Querydsl4RepositorySupport(Class<?> domainClass) {
-        Assert.notNull(domainClass, "Domain class must not be null");
-        this.domainClass = domainClass;
+  public Querydsl4RepositorySupport(Class<?> domainClass) {
+    Assert.notNull(domainClass, "Domain class must not be null");
+    this.domainClass = domainClass;
+  }
+
+  @Autowired
+  public void setEntityManager(EntityManager entityManager) {
+    Assert.notNull(entityManager, "EntityManager must not be null");
+    JpaEntityInformation<?, ?> entityInformation =
+        JpaEntityInformationSupport.getEntityInformation(domainClass, entityManager);
+    SimpleEntityPathResolver resolver = SimpleEntityPathResolver.INSTANCE;
+    EntityPath<?> path = resolver.createPath(entityInformation.getJavaType());
+    this.entityManager = entityManager;
+    this.querydsl = new Querydsl(entityManager,
+        new PathBuilder<>(path.getType(), path.getMetadata()));
+    this.queryFactory = new JPAQueryFactory(entityManager);
+  }
+
+  protected JPAQueryFactory getQueryFactory() {
+    return queryFactory;
+  }
+
+  protected Querydsl getQuerydsl() {
+    return querydsl;
+  }
+
+  protected EntityManager getEntityManager() {
+    return entityManager;
+  }
+
+  protected <T> JPAQuery<T> select(Expression<T> expression) {
+    return getQueryFactory().select(expression);
+  }
+
+  protected <T> JPAQuery<T> selectFrom(EntityPath<T> from) {
+    return getQueryFactory().selectFrom(from);
+  }
+
+  protected <T> Page<T> applyPagination(Pageable pageable,
+      Function<JPAQueryFactory, JPAQuery<T>> contentQuery,
+      Function<JPAQueryFactory, JPAQuery<Long>> countQuery) {
+    JPAQuery<T> jpaCountQuery = contentQuery.apply(getQueryFactory());
+    List<T> content = getQuerydsl().applyPagination(pageable, jpaCountQuery).fetch();
+    JPAQuery<Long> countResult = countQuery.apply(getQueryFactory());
+    LongSupplier totalSupplier = () -> {
+      Long count = countResult.fetchOne();
+      return count != null ? count : 0L;
+    };
+    return PageableExecutionUtils.getPage(content, pageable, totalSupplier);
+  }
+
+  public <T> JPQLQuery<T> applyPagination(Pageable pageable, JPQLQuery<T> query) {
+    Assert.notNull(pageable, "Pageable must not be null");
+    Assert.notNull(query, "Pageable must not be null");
+
+    if (pageable.isUnpaged()) {
+      return query;
     }
 
-    @Autowired
-    public void setEntityManager(EntityManager entityManager) {
-        Assert.notNull(entityManager, "EntityManager must not be null");
-        JpaEntityInformation entityInformation =
-                JpaEntityInformationSupport.getEntityInformation(domainClass, entityManager);
-        SimpleEntityPathResolver resolver = SimpleEntityPathResolver.INSTANCE;
-        EntityPath path = resolver.createPath(entityInformation.getJavaType());
-        this.entityManager = entityManager;
-        this.querydsl = new Querydsl(entityManager, new PathBuilder<>(path.getType(), path.getMetadata()));
-        this.queryFactory = new JPAQueryFactory(entityManager);
+    query.offset(pageable.getOffset());
+    query.limit(pageable.getPageSize());
+
+    return getQuerydsl().applySorting(pageable.getSort(), query);
+  }
+
+  protected <T> Slice<T> applySlicing(Pageable pageable,
+      Function<JPAQueryFactory, JPAQuery<T>> contentQuery) {
+    JPAQuery<T> jpaQuery = contentQuery.apply(getQueryFactory());
+    jpaQuery.offset(pageable.getOffset());
+    jpaQuery.limit(pageable.getPageSize() + 1);
+
+    List<T> content = getQuerydsl().applySorting(pageable.getSort(), jpaQuery).fetch();
+
+    hasNext = false;
+    if (content.size() > pageable.getPageSize()) {
+      content.remove(pageable.getPageSize());
+      hasNext = true;
     }
 
-    protected JPAQueryFactory getQueryFactory() {
-        return queryFactory;
-    }
-
-    protected Querydsl getQuerydsl() {
-        return querydsl;
-    }
-
-    protected EntityManager getEntityManager() {
-        return entityManager;
-    }
-
-    protected <T> JPAQuery<T> select(Expression<T> expression) {
-        return getQueryFactory().select(expression);
-    }
-
-    protected <T> JPAQuery<T> selectFrom(EntityPath<T> from) {
-        return getQueryFactory().selectFrom(from);
-    }
-
-    protected <T> Page<T> applyPagination(Pageable pageable, Function<JPAQueryFactory, JPAQuery> contentQuery,
-                                          Function<JPAQueryFactory, JPAQuery> countQuery) {
-        JPAQuery jpaCountQuery = contentQuery.apply(getQueryFactory());
-        List<T> content = getQuerydsl().applyPagination(pageable, jpaCountQuery).fetch();
-        JPAQuery countResult = countQuery.apply(getQueryFactory());
-        return PageableExecutionUtils.getPage(content, pageable, countResult::fetchCount);
-    }
-
-    public <T> JPQLQuery<T> applyPagination(Pageable pageable, JPQLQuery<T> query) {
-        Assert.notNull(pageable, "Pageable must not be null");
-        Assert.notNull(query, "Pageable must not be null");
-
-        if (pageable.isUnpaged()) {
-            return query;
-        }
-
-        query.offset(pageable.getOffset());
-        query.limit(pageable.getPageSize());
-
-        return getQuerydsl().applySorting(pageable.getSort(), query);
-    }
-
-    protected <T> Slice<T> applySlicing(Pageable pageable, Function<JPAQueryFactory, JPAQuery> contentQuery) {
-        JPAQuery jpaQuery = contentQuery.apply(getQueryFactory());
-        jpaQuery.offset(pageable.getOffset());
-        jpaQuery.limit(pageable.getPageSize() + 1);
-
-        List<T> content = getQuerydsl().applySorting(pageable.getSort(), jpaQuery).fetch();
-
-        hasNext = false;
-        if (content.size() > pageable.getPageSize()) {
-            content.remove(pageable.getPageSize());
-            hasNext = true;
-        }
-
-        return new SliceImpl<>(content, pageable, hasNext);
-    }
+    return new SliceImpl<>(content, pageable, hasNext);
+  }
 }
